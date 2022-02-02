915b8b7a48cb5e8e68a92af5ec907baaf2aedbff
romerodevelopments <gandresrp@gmail.com>
2022-02-01 23:54:50 -0300

    pruebas unitarias sobre un proyecto existente - caso de prueba del boton

--- SnapShot-testing/src/__tests__/Form.test.js
+++ SnapShot-testing/src/__tests__/Form.test.js
@@ -24,4 +24,15 @@ describe("<Form />", () => {
     /* //toJson() devuelve un objeto que representa el dom  rederizado en formato json
     console.log(component.toJSON()); */
   });
+
+  it("El boton debe habilitarse si el input deja de estar vacío", () => {
+    const form = component.root.findByType("form");
+    const input = form.findByType("input");
+    const button = form.findByType("button");
+
+    /* console.log(button.props); */
+    //Confirmamos que el botón venga desacgtivado y tenga la clase null
+    expect(button.props.disabled).toBeTruthy();
+    expect(button.props.className).toEqual("search-button null");
+  });
 })
