437c59f104443c964bcb8b738349f32383e6db4b
romerodevelopments <gandresrp@gmail.com>
2022-02-04 02:25:25 -0300

    mocking en unit testing con jest - actualizar mocking y componente

--- last-project/src/components/SampleComponent.test.js
+++ last-project/src/components/SampleComponent.test.js
@@ -26,12 +26,32 @@ describe("<SampleComponent", () => {
     expect(window.fetch).not.toHaveBeenCalled();
 
     await act(async () => {
-      //Ejecuta los timers de jest
+      //Ejecuta los timers de jest lo que permite que se gestioné la llamada a la api
       await jest.runAllTimers();
     });
     // console.log(window.fetch);
 
     //Valida que la función fetch ejecutada en SampleComponent este siendo llamada
+    expect(window.fetch).toHaveBeenCalled();
+    expect(window.fetch).toHaveBeenCalledTimes(2);
+
+    expect(component.root.findAllByType("p").length).toEqual(0);
+
+    //Reescribe fetch para recibir la data de un usuario, esto implica que el contador de llamadas se reinicie en 0
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve([{id: 1, name: "Gustavo Romero"}])
+    }));
+
+    await act(async () => {
+      //Ejecuta los timers nuevamente lo que permite que se gestione la llamada a la api con
+      await jest.runAllTimers();
+    });
+
+    expect(component.root.findAllByType("p").length).toEqual(1);
+
+    //Actualiza el componente para que los cambios en la data recibida se actualicen
+    await component.update(<SampleComponent />);
+
     expect(window.fetch).toHaveBeenCalled();
     expect(window.fetch).toHaveBeenCalledTimes(2);
   })
